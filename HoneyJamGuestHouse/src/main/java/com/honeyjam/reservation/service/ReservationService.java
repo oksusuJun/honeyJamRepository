19583499b7ed6615f8217e92bdc597f19da3f526
LeeSoojung <soolee9017@gmail.com>
2017-11-01 15:55:13 +0900

    .

--- HoneyJamGuestHouse/src/main/java/com/honeyjam/reservation/service/ReservationService.java
+++ HoneyJamGuestHouse/src/main/java/com/honeyjam/reservation/service/ReservationService.java
@@ -12,7 +12,8 @@ public interface ReservationService {
 	
 	Map<String,Integer> selectReservationByDate(java.sql.Date date); 
 	
-	List<String> emptyRoomsByDate(String checkin, String checkout) throws IOException, ParseException;
 
 	int dayBetween(String checkin, String checkout) throws ParseException;
+	
+	List<String> emptyRoomsByDate(int guests, String checkin, String checkout) throws IOException, ParseException;
 }
