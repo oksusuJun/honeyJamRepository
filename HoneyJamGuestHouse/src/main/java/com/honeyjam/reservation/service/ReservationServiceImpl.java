62196443cf84586348a5023da7e235e23d177dfd
HyeonJun <phj6207@naver.com>
2017-11-03 10:32:34 +0900

    예약취소 완성

--- HoneyJamGuestHouse/src/main/java/com/honeyjam/reservation/service/ReservationServiceImpl.java
+++ HoneyJamGuestHouse/src/main/java/com/honeyjam/reservation/service/ReservationServiceImpl.java
@@ -317,7 +317,7 @@ public class ReservationServiceImpl implements ReservationService{
 	}
 
 	@Override
-	public int deleteReservationById(int reservationId, String checkIn) {
+	public int deleteReservationByEmailDate(int reservationId, String checkIn) {
 		SqlSession session = null;
 		List<Object> list = new ArrayList<>();
 		
@@ -329,7 +329,7 @@ public class ReservationServiceImpl implements ReservationService{
 				checkInForm = dateForm.parse(checkIn);
 				list.add(reservationId);
 				list.add(checkInForm);
-				int deleteRow = dao.deleteReservationById(session, list);
+				int deleteRow = dao.deleteReservationByEmailDate(session, list);
 				
 				session = factory.openSession();
 				session.commit();
@@ -344,6 +344,20 @@ public class ReservationServiceImpl implements ReservationService{
 			
 			return 0;
 		
+	}
+
+	@Override
+	public int deleteReservationByNum(int reservationId) {
+		SqlSession session = null;
+		try {
+			session = factory.openSession();
+			int result = dao.deleteReservationById(session, reservationId);
+			System.out.println("ServiceImpl - reservationId : " + reservationId +"/ result : " + result);
+			session.commit();
+			return result;
+		} finally {
+			session.close();
+		}
 	}
 }
 
